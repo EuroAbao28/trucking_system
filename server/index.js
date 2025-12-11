d12d8ed2dacc28bbbc2aeb7710c1d28e286acc1a
Euro Abao <abaoeuro2002@gmail.com>
2025-12-11 11:14:29 +0800

    cors and json changes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,11 @@ connectDB()
 
 // middleware
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: ['https://ebun-freight.vercel.app/', 'http://localhost:5173/']
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
