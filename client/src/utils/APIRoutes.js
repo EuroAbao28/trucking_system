a180e7b3fa9b79394016932c83b1013f97776ced
Euro Abao <abaoeuro2002@gmail.com>
2025-12-11 11:36:30 +0800

    from local to live

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,23 +1,24 @@
 // host
 export const LOCAL = 'http://localhost:5000'
+export const LIVE = 'https://ebun-freight.onrender.com'
 
 // truck
-export const API_TRUCK = `${LOCAL}/api/truck`
+export const API_TRUCK = `${LIVE}/api/truck`
 
 // driver
-export const API_DRIVER = `${LOCAL}/api/driver`
+export const API_DRIVER = `${LIVE}/api/driver`
 
 // admin
-export const API_USER = `${LOCAL}/api/user`
+export const API_USER = `${LIVE}/api/user`
 
 // tool
-export const API_TOOL = `${LOCAL}/api/tool`
+export const API_TOOL = `${LIVE}/api/tool`
 
 // deployment
-export const API_DEPLOYMENT = `${LOCAL}/api/deployment`
+export const API_DEPLOYMENT = `${LIVE}/api/deployment`
 
 // analytics
-export const API_ANALYTICS = `${LOCAL}/api/analytics`
+export const API_ANALYTICS = `${LIVE}/api/analytics`
 
 //  access
-export const API_CLIENT = `${LOCAL}/api/client`
+export const API_CLIENT = `${LIVE}/api/client`
